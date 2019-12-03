07ad3b3bccc1a3c97e2130de6b8b333872c1751a
naveen <naveen@krify.net>
2019-12-03 17:49:18 +0530

    fix4

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('push', function (e) {
+    console.log(e.data)
     var options = {
         body: 'This notification was generated from a push!',
         icon: 'images/example.png',
